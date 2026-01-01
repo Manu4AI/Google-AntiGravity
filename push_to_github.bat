bd4b1bbe9cd52f6e8af53986945825da8649a881
Shivendra Gupta <gshiv009@gmail.com>
2026-01-01 19:26:50 +0530

    Manual Update: Thu 01/01/2026 19:26:49.91

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,17 +1,18 @@
 @echo off
 echo Syncing to GitHub...
 
-:: Ensure Secrets are NOT tracked
+:: Consolidate cleaning secrets
 git rm --cached "Script RSI Calculation/service_account.json" >nul 2>&1
 git rm --cached "Telegram Integration/telegram_credentials.json" >nul 2>&1
 git rm --cached "service_account.json" >nul 2>&1
 git rm --cached "telegram_credentials.json" >nul 2>&1
 
-:: Add & Commit
+:: 1. Save local changes
 git add .
 git commit -m "Manual Update: %DATE% %TIME%"
 
-:: Push (Standard)
+:: 2. Push to GitHub (Standard push, will fail if remote has new changes)
+echo Pushing...
 git push origin main
 
 echo Done.
